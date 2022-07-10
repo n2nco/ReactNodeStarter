e5499e306dcd46125ba0ad1987f4183eb0b103a8
Nielsen <askprogrammers@gmail.com>
2022-07-10 17:32:53 -0400

    first commit

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+//write an express node server
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/yo', (req, res) => res.send('Hello Worldings!'))
